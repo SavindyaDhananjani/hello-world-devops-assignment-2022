0409e7b141470c73e4669814581cdda9fca377ce
SavindyaDhananjani <savindyadhananjani@gmail.com>
2022-10-16 20:53:23 +0530

    add changes

---
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -0,0 +1,22 @@
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RequestMapping(value="/hello-world")
+@RestController
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @GetMapping("/welcome")
+    public String HelloWorld() {
+        return String.format("Welcome to LOIT");
+    }
+}
